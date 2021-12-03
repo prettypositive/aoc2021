286c660b27061ab1f310d34f883f6285e17bfab7
prettypositive <jmquander@gmail.com>
2021-12-03 11:07:44 -0500

    part 2

--- day 03/part2.cpp
+++ day 03/part2.cpp
@@ -13,9 +13,9 @@ auto parse_input() {
     return numbers;
 }
 
-auto most_common_bit(const vector<string>& numbers, int position) {
+char most_common_bit(const vector<string>& numbers, int position) {
     int zeroes = 0, ones = 0;
-    for (const auto& number : numbers) {
+    for (const string& number : numbers) {
         if (number[position] == '0')
             zeroes += 1;
         else if (number[position] == '1')
@@ -27,14 +27,14 @@ auto most_common_bit(const vector<string>& numbers, int position) {
         return '1';
 }
 
-auto least_common_bit(const vector<string>& numbers, int position) {
+char least_common_bit(const vector<string>& numbers, int position) {
     if (most_common_bit(numbers, position) == '0')
         return '1';
     else
         return '0';
 }
 
-auto find_rating(vector<string> numbers, const string& rating_type) {
+string find_rating(vector<string> numbers, const string& rating_type) {
     for (int i = 0; i < numbers[0].size(); i++) {
         char target;
         if (rating_type == "oxygen")
@@ -42,7 +42,7 @@ auto find_rating(vector<string> numbers, const string& rating_type) {
         else if (rating_type == "co2")
             target = least_common_bit(numbers, i);
         vector<string> good_numbers;
-        for (const auto& number : numbers) {
+        for (const string& number : numbers) {
             if (number[i] == target) good_numbers.push_back(number);
         }
         numbers = good_numbers;
@@ -53,8 +53,8 @@ auto find_rating(vector<string> numbers, const string& rating_type) {
 
 int main() {
     auto numbers = parse_input();
-    auto oxygen_rating = find_rating(numbers, "oxygen");
-    auto co2_rating = find_rating(numbers, "co2");
+    string oxygen_rating = find_rating(numbers, "oxygen");
+    string co2_rating = find_rating(numbers, "co2");
     cout << stoi(oxygen_rating, nullptr, 2) * stoi(co2_rating, nullptr, 2);
     return 0;
 }
