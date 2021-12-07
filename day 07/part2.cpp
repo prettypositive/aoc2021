f65d9fc11082c8aa21a5dced7f3f2f6422b9f3d1
prettypositive <jmquander@gmail.com>
2021-12-07 08:29:52 -0500

    brute force is good enough i guess

---
+++ day 07/part2.cpp
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+auto parse_input() {
+    ifstream input("input.txt");
+    string position;
+    vector<int> positions;
+    while (getline(input, position, ',')) {
+        positions.push_back(stoi(position));
+    }
+    return positions;
+}
+
+int main() {
+    auto start = chrono::steady_clock::now();
+
+    auto positions = parse_input();
+    auto highest = *max_element(positions.begin(), positions.end());
+    vector<int> fuel;
+    fuel.resize(highest);
+    for (int i = 0; i < fuel.size(); i++) {
+        for (const auto& position : positions) {
+            int cost = 0;
+            for (int j = 1; j < abs(position - i) + 1; j++) {
+                cost += j;
+            }
+            fuel[i] += cost;
+        }
+    }
+    cout << *min_element(fuel.begin(), fuel.end());
+
+    auto end = chrono::steady_clock::now();
+    cout << " [" << chrono::duration<double, milli>(end - start).count()
+         << " ms]" << endl;
+    return 0;
+}
