a21ff8da825b8cd43946905a5ee1ed0cde4579b3
prettypositive <jmquander@gmail.com>
2021-12-10 10:20:59 -0500

    flattened some indents

--- day 10/part2.cpp
+++ day 10/part2.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <iostream>
 #include <iterator>
+#include <tuple>
 #include <unordered_map>
 #include <vector>
 using namespace std;
@@ -14,37 +15,27 @@ auto parse_input() {
     return lines;
 }
 
-bool is_valid_line(const string& line, vector<char>& stack) {
-    unordered_map<char, char> targets = {
+tuple<bool, vector<char>> parse_line(const string& line) {
+    vector<char> stack;
+    unordered_map<char, char> pairs = {
         {'(', ')'}, {'[', ']'}, {'{', '}'}, {'<', '>'}};
-    char target, killer = '0';
+    char target;
+    bool valid = true;
     for (const auto& character : line) {
-        switch (character) {
-            case '(':
-            case '[':
-            case '{':
-            case '<':
-                target = targets[character];
-                stack.push_back(character);
-                break;
-            case ')':
-            case ']':
-            case '}':
-            case '>':
-                if (character != target) {
-                    killer = character;
-                } else {
-                    stack.pop_back();
-                    target = targets[stack.back()];
-                }
-                break;
+        if (pairs.contains(character)) {
+            target = pairs[character];
+            stack.push_back(character);
+        } else {
+            valid = (character == target);
+            if (!valid) break;
+            stack.pop_back();
+            target = pairs[stack.back()];
         }
-        if (killer != '0') break;
     }
-    return (killer == '0') ? true : false;
+    return {valid, stack};
 }
 
-int64_t score_line(const string& line, const vector<char>& stack) {
+auto score_line(const string& line, const vector<char>& stack) {
     unordered_map<char, int> score_lookup = {
         {'(', 1}, {'[', 2}, {'{', 3}, {'<', 4}};
     int64_t score = 0;
@@ -59,11 +50,8 @@ int64_t solve_puzzle() {
     auto lines = parse_input();
     vector<int64_t> scores;
     for (const auto& line : lines) {
-        vector<char> stack;
-        if (is_valid_line(line, stack)) {
-            auto score = score_line(line, stack);
-            scores.push_back(score);
-        }
+        auto [valid, stack] = parse_line(line);
+        if (valid) scores.push_back(score_line(line, stack));
     }
     sort(scores.begin(), scores.end());
     return scores[scores.size() / 2];
