08313540d605e5f75a9c0d70f1da57545ffc8ccc
prettypositive <jmquander@gmail.com>
2021-12-16 13:00:40 -0500

    still needs some refactoring

--- day 16/part2.cpp
+++ day 16/part2.cpp
@@ -45,6 +45,17 @@ std::string hex_char_to_binary(char c) {
     return "";
 }
 
+enum Op {
+    SUM = 0,
+    PRODUCT = 1,
+    MINIMUM = 2,
+    MAXIMUM = 3,
+    LITERAL = 4,
+    GREATER = 5,
+    LESS = 6,
+    EQUAL = 7
+};
+
 auto parse_input() {
     std::ifstream input("input.txt");
     char c;
@@ -56,14 +67,15 @@ auto parse_input() {
     return packet;
 }
 
-std::tuple<int64_t, int> parse_packet(std::string& packet) {
-    auto start = packet.begin();
-    auto it = start;
-    int64_t number;
-    std::vector<int64_t> numbers;
+std::tuple<int64_t, int> parse_packet(const std::string& packet) {
+    auto start = packet.begin(), it = start;
+    int64_t value;
+    std::vector<int64_t> values;
+    int version = stoi(std::string(it, it + 3), nullptr, 2);
     int packet_type = stoi(std::string(it + 3, it + 6), nullptr, 2);
     it += 6;
-    if (packet_type == 4) {
+
+    if (packet_type == Op::LITERAL) {
         std::string number_bits;
         while (*it == '1') {
             number_bits += std::string(it + 1, it + 5);
@@ -71,62 +83,60 @@ std::tuple<int64_t, int> parse_packet(std::string& packet) {
         }
         number_bits += std::string(it + 1, it + 5);
         it += 5;
-        number = stoll(number_bits, nullptr, 2);
-        return {number, (it - start)};
+        value = stoll(number_bits, nullptr, 2);
+        return {value, (it - start)};
 
     } else if (*it == '0') {
-        it += 1;
-        int total_bits = stoi(std::string(it, it + 15), nullptr, 2);
-        it += 15;
+        int total_bits = stoi(std::string(it + 1, it + 16), nullptr, 2);
+        it += 16;
         int used_bits = 0;
         std::string packets(it, it + total_bits);
         while (used_bits < total_bits) {
-            auto [number, new_used_bits] = parse_packet(packets);
-            used_bits += new_used_bits;
-            numbers.push_back(number);
+            auto [value, length] = parse_packet(packets);
+            used_bits += length;
+            values.push_back(value);
             packets.assign(it + used_bits, it + total_bits);
         }
         it += total_bits;
 
     } else if (*it == '1') {
-        it += 1;
-        int total_packets = stoi(std::string(it, it + 11), nullptr, 2);
-        it += 11;
+        int total_packets = stoi(std::string(it + 1, it + 12), nullptr, 2);
+        it += 12;
         int used_bits = 0;
         std::string packets(it, packet.end());
         for (int i = 0; i < total_packets; i++) {
-            auto [number, new_used_bits] = parse_packet(packets);
-            used_bits += new_used_bits;
-            numbers.push_back(number);
+            auto [value, length] = parse_packet(packets);
+            used_bits += length;
+            values.push_back(value);
             packets.assign(it + used_bits, packet.end());
         }
         it += used_bits;
     }
 
-    if (packet_type == 0) {
-        number = std::accumulate(numbers.begin(), numbers.end(), 0LL);
-    } else if (packet_type == 1) {
-        number = std::accumulate(numbers.begin(), numbers.end(), 1LL,
-                                 std::multiplies<int64_t>());
-    } else if (packet_type == 2) {
-        number = *std::min_element(numbers.begin(), numbers.end());
-    } else if (packet_type == 3) {
-        number = *std::max_element(numbers.begin(), numbers.end());
-    } else if (packet_type == 5) {
-        number = (numbers[0] > numbers[1]) ? 1 : 0;
-    } else if (packet_type == 6) {
-        number = (numbers[0] < numbers[1]) ? 1 : 0;
-    } else if (packet_type == 7) {
-        number = (numbers[0] == numbers[1]) ? 1 : 0;
+    if (packet_type == Op::SUM) {
+        value = std::accumulate(values.begin(), values.end(), 0LL);
+    } else if (packet_type == Op::PRODUCT) {
+        value = std::accumulate(values.begin(), values.end(), 1LL,
+                                std::multiplies<int64_t>());
+    } else if (packet_type == Op::MINIMUM) {
+        value = *std::min_element(values.begin(), values.end());
+    } else if (packet_type == Op::MAXIMUM) {
+        value = *std::max_element(values.begin(), values.end());
+    } else if (packet_type == Op::GREATER) {
+        value = (values[0] > values[1]) ? 1 : 0;
+    } else if (packet_type == Op::LESS) {
+        value = (values[0] < values[1]) ? 1 : 0;
+    } else if (packet_type == Op::EQUAL) {
+        value = (values[0] == values[1]) ? 1 : 0;
     }
 
-    return {number, (it - start)};
+    return {value, (it - start)};
 }
 
 auto solve_puzzle() {
     auto packet = parse_input();
-    auto [number, _] = parse_packet(packet);
-    return number;
+    auto [solution, _] = parse_packet(packet);
+    return solution;
 }
 
 int main() {
