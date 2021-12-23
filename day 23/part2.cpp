f4b44c8fd391f2766eff7ec999e5d844539c67a9
prettypositive <jmquander@gmail.com>
2021-12-23 13:39:55 -0500

    part 2 slow dfs

--- day 23/part2.cpp
+++ day 23/part2.cpp
@@ -1,9 +1,22 @@
 #include <bits/stdc++.h>
 
+std::unordered_map<char, std::unordered_map<int, int>> move_costs = {
+    {'A', {{0, 6}, {1, 5}, {2, 5}, {3, 7}, {4, 9}, {5, 11}, {6, 12}}},
+    {'B', {{0, 8}, {1, 7}, {2, 5}, {3, 5}, {4, 7}, {5, 9}, {6, 10}}},
+    {'C', {{0, 10}, {1, 9}, {2, 7}, {3, 5}, {4, 5}, {5, 7}, {6, 8}}},
+    {'D', {{0, 12}, {1, 11}, {2, 9}, {3, 7}, {4, 5}, {5, 5}, {6, 6}}}};
+std::unordered_map<char, int> piece_costs = {
+    {'A', 1}, {'B', 10}, {'C', 100}, {'D', 1000}};
+
 struct GameState {
     // A between 1-2, B between 2-3, C between 3-4, D between 4-5
     std::array<char, 7> hallway = {};
     std::unordered_map<char, std::vector<char>> homes;
+    GameState* solved;
+
+    bool operator==(const GameState& other) const {
+        return (hallway == other.hallway && homes == other.homes);
+    }
 };
 
 struct Move {
@@ -13,18 +26,8 @@ struct Move {
     int cost;
 };
 
-bool is_solved(const GameState& game) {
-    if (game.homes.at('A') == std::vector{'A', 'A', 'A', 'A'} &&
-        game.homes.at('B') == std::vector{'B', 'B', 'B', 'B'} &&
-        game.homes.at('C') == std::vector{'C', 'C', 'C', 'C'} &&
-        game.homes.at('D') == std::vector{'D', 'D', 'D', 'D'})
-        return true;
-    else
-        return false;
-}
-
-bool is_path_clear(const GameState& game, std::pair<char, int> source,
-                   std::pair<char, int> destination) {
+bool is_path_clear(const GameState& game, const std::pair<char, int>& source,
+                   const std::pair<char, int>& destination) {
     std::unordered_map<char, std::unordered_map<int, std::vector<int>>>
         blockers;
     blockers['A'] = {{0, {1}},    {1, {}},        {2, {}},          {3, {2}},
@@ -51,18 +54,14 @@ bool is_path_clear(const GameState& game, std::pair<char, int> source,
 void find_hallway_moves(const GameState& game, std::vector<Move>& legal_moves) {
     // pieces in a hallway can only move to their designated home
     // and only if the home is homogenous with their type
-    std::unordered_map<char, std::unordered_map<int, int>> move_costs;
-    move_costs['A'] = {{0, 6}, {1, 5},  {2, 5}, {3, 7},
-                       {4, 9}, {5, 11}, {6, 12}};
-    move_costs['B'] = {{0, 8}, {1, 7}, {2, 5}, {3, 5}, {4, 7}, {5, 9}, {6, 10}};
-    move_costs['C'] = {{0, 10}, {1, 9}, {2, 7}, {3, 5}, {4, 5}, {5, 7}, {6, 8}};
-    move_costs['D'] = {{0, 12}, {1, 11}, {2, 9}, {3, 7},
-                       {4, 5},  {5, 5},  {6, 6}};
-    std::unordered_map<char, int> piece_costs = {
-        {'A', 1}, {'B', 10}, {'C', 100}, {'D', 1000}};
-
     for (int i = 0; i < game.hallway.size(); i++) {
         if (!game.hallway[i]) continue;
+        bool valid_home = true;
+        for (const auto& piece : game.homes.at(game.hallway[i])) {
+            if (piece != game.hallway[i]) valid_home = false;
+        }
+        if (!valid_home) continue;
+
         Move move;
         move.source = {'H', i};
         move.piece = game.hallway[i];
@@ -70,32 +69,15 @@ void find_hallway_moves(const GameState& game, std::vector<Move>& legal_moves) {
         move.cost = (move_costs[move.destination.first][i] -
                      game.homes.at(move.piece).size()) *
                     piece_costs[move.piece];
-
-        bool valid_home = true;
-        for (const auto& piece : game.homes.at(move.piece)) {
-            if (piece != move.piece) valid_home = false;
-        }
-        if (!valid_home) continue;
-
         if (is_path_clear(game, move.source, move.destination))
             legal_moves.push_back(move);
     }
 }
 
-void find_home_moves(const GameState& game, char home,
+void find_home_moves(const GameState& game, const char home,
                      std::vector<Move>& legal_moves) {
     // pieces in a home can only move to a hallway space (not really, but it
     // simplifies things)
-    std::unordered_map<char, std::unordered_map<int, int>> move_costs;
-    move_costs['A'] = {{0, 6}, {1, 5},  {2, 5}, {3, 7},
-                       {4, 9}, {5, 11}, {6, 12}};
-    move_costs['B'] = {{0, 8}, {1, 7}, {2, 5}, {3, 5}, {4, 7}, {5, 9}, {6, 10}};
-    move_costs['C'] = {{0, 10}, {1, 9}, {2, 7}, {3, 5}, {4, 5}, {5, 7}, {6, 8}};
-    move_costs['D'] = {{0, 12}, {1, 11}, {2, 9}, {3, 7},
-                       {4, 5},  {5, 5},  {6, 6}};
-    std::unordered_map<char, int> piece_costs = {
-        {'A', 1}, {'B', 10}, {'C', 100}, {'D', 1000}};
-
     if (game.homes.at(home).empty()) return;
     bool homogenous = true;
     for (const auto& piece : game.homes.at(home)) {
@@ -117,7 +99,7 @@ void find_home_moves(const GameState& game, char home,
     }
 }
 
-GameState make_move(GameState game, const Move move) {
+GameState make_move(GameState game, const Move& move) {
     if (move.source.first == 'H') {
         game.hallway[move.source.second] = false;
     } else {
@@ -132,8 +114,8 @@ GameState make_move(GameState game, const Move move) {
 }
 
 void dfs_solve(const GameState& game, int& best_score, const int& score = 0) {
-    if (score > best_score) return;
-    if (is_solved(game)) {
+    if (score >= best_score) return;
+    if (game == *game.solved) {
         if (score < best_score) std::cout << score << std::endl;
         best_score = score;
         return;
@@ -143,7 +125,6 @@ void dfs_solve(const GameState& game, int& best_score, const int& score = 0) {
     for (const auto& [home, _] : game.homes) {
         find_home_moves(game, home, legal_moves);
     }
-    if (legal_moves.empty()) return;
     for (const auto& move : legal_moves) {
         dfs_solve(make_move(game, move), best_score, score + move.cost);
     }
@@ -151,11 +132,16 @@ void dfs_solve(const GameState& game, int& best_score, const int& score = 0) {
 }
 
 auto solve_puzzle() {
-    GameState game;
+    GameState game, solved;
     game.homes['A'] = {'C', 'D', 'D', 'B'};
     game.homes['B'] = {'C', 'B', 'C', 'B'};
     game.homes['C'] = {'A', 'A', 'B', 'D'};
     game.homes['D'] = {'A', 'C', 'A', 'D'};
+    solved.homes['A'] = {'A', 'A', 'A', 'A'};
+    solved.homes['B'] = {'B', 'B', 'B', 'B'};
+    solved.homes['C'] = {'C', 'C', 'C', 'C'};
+    solved.homes['D'] = {'D', 'D', 'D', 'D'};
+    game.solved = &solved;
     int best_score = INT_MAX;
     dfs_solve(game, best_score);
     return best_score;
